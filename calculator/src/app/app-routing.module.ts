66d8344793f92cb4b79b25aac3ed1d140e0ccfcb
rushi2163 <rushikeshjadhav7410135881@gmail.com>
2023-10-13 11:45:40 +0530

    calculate component is added

--- calculator/src/app/app-routing.module.ts
+++ calculator/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculateComponent } from './calculate/calculate.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:CalculateComponent,title:"Calculation Page"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
