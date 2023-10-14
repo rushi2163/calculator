d5290253445ed332895f9cf3e242bd689592d5a6
rushi2163 <rushikeshjadhav7410135881@gmail.com>
2023-10-15 00:00:58 +0530

    functionalityes are added

--- calculator/src/app/app.module.ts
+++ calculator/src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { CalculateComponent } from './calculate/calculate.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +18,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- calculator/src/app/calculate/calculate.component.ts
+++ calculator/src/app/calculate/calculate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { every } from 'rxjs';
 
 @Component({
   selector: 'app-calculate',
@@ -8,8 +9,40 @@ import { Component, OnInit } from '@angular/core';
 export class CalculateComponent implements OnInit {
 
   constructor() { }
+  buttons=['C','Del','%','/',7,8,9,'*',4,5,6,'-',1,2,3,'+','.',0,'=']
+  answer:string='0'
+  current:string=''
+  calculate(value:any){
+    if(value!='C'&&value!='Del'&&value!='='){
+      this.current+=value
 
+      this.answer=this.current
+    }
+    if(value=='Del'){
+      if(this.answer.length>1){
+          this.answer=this.answer.substring(0,this.answer.length-1)
+        }
+      else{
+        this.answer='0'
+        this.current=''
+      }
+    }
+    if(value=='C'){
+      this.answer='0'
+      this.current=''
+    }
+    if(value=='='){
+      this.current=eval(this.answer)
+      this.answer=this.current
+      console.log(eval(this.answer));
+
+      
+    }
+    //&&value!='%'&&value!='/'&&value!='*'&&value!='-'&&value!='+'
+    
+  }
   ngOnInit(): void {
+
   }
 
 }
